78267d0d7c003c61d7e6eae21c67a859ad7e1b98
IvanCCO <ivanmedeiros0903@outlook.com>
2024-02-14 15:50:53 +0000

    Import discord go api

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func main() {
+
+	discord, err := discordgo.New("Bot " + "authentication token")
+
+	fmt.Println(discord.Client)
+	fmt.Println(err)
+
 	fmt.Println("Hello, World!")
 }
