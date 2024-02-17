29f2368fad0f79d0dc82beb18f6cec6b0191b567
IvanCCO <ivanmedeiros0903@outlook.com>
2024-02-17 03:49:24 +0000

    commitando

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"fmt"
+	_ "fmt"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
-
-	discord, err := discordgo.New("Bot " + "authentication token")
-
-	fmt.Println(discord.Client)
-	fmt.Println(err)
-
-	fmt.Println("Hello, World!")
+	discordgo.New("Bot " + "authentication token")
 }
